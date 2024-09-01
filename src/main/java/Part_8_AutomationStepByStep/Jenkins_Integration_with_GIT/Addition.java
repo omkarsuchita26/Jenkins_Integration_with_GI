2af94e3ebb29c85cf870044908b7811e03676065
Omkar <omkark26@gmail.com>
2024-09-01 20:11:58 +0530

    Addition Project added

---
+++ src/main/java/Part_8_AutomationStepByStep/Jenkins_Integration_with_GIT/Addition.java
@@ -0,0 +1,9 @@
+package Part_8_AutomationStepByStep.Jenkins_Integration_with_GIT;
+
+public class Addition {
+	public static void main(String[] args) {
+		int a=10, b=20;
+		int c=a+b;
+		System.out.println("Addition of 10 + 20 is " + c);		
+	}
+}
